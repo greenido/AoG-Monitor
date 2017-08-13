b6dd2b50d5236212c5f783a64e547538e9af02ff
Ido Green <greenido@gmail.com>
2017-08-12 19:04:54 -0700

    Adding reports and the ability to fetch only new issues

--- test.php
+++ test.php
@@ -13,24 +13,38 @@
 
 require_once 'vendor/autoload.php';
 $client = new \Github\Client();
+date_default_timezone_set('America/Los_Angeles');
 
 //
 // Fetch all the open issues per $repo
 //
 function printIssues($repo, $client) {
-  echo "\n\nIssues on $repo\n";
-  echo "===========================================\n\n";
+  $twoDaysAgoTime = strtotime("-2 days");
+  $twoDaysAgoStr = date('Y-m-d', $twoDaysAgoTime);
+
+  echo "\n\nIssues on $repo from $twoDaysAgoStr\n";
+  echo "==========================================================\n\n";
   $issues = $client->api('issue')->all('actions-on-google', $repo, array('state' => 'open'));
   $i = 1;
   foreach ($issues as $key => $issue) {
     $tmpDate = substr($issue['created_at'], 0, 10);
-    echo $i . ") [" . $tmpDate . "] " . ($issue['title'] . " - " . $issue['url'] .  "\n");
-    $i++;
+    if (strtotime($tmpDate) > $twoDaysAgoTime) {
+      echo $i . ") [" . $tmpDate . "] " . ($issue['title'] . " - " . $issue['url'] .  "\n");
+      $i++;
+    }
   }
 }
 
+$reposToCheck = ['actions-on-google-nodejs',
+  'apiai-facts-about-google-nodejs',
+  'actionssdk-smart-home-nodejs',
+  'apiai-trivia-game-nodejs',
+  'apiai-transactions-nodejs',
+  'apiai-webhook-template-nodejs'];
 // All the repos we wish to monitor
-printIssues('actions-on-google-nodejs', $client);
-printIssues('actionssdk-smart-home-nodejs', $client);
+foreach ($reposToCheck as $key => $repoName) {
+  printIssues($repoName, $client);  
+}
 
+echo "\n\n**** Done **** \n";
 
